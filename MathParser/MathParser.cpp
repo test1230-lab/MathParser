218b54525eff7585cc86ea3ca9a83183bba8dd22
test <boomerdoodle149@gmail.com>
2021-02-20 00:40:11 -0500

    pre

--- MathParser/MathParser.cpp
+++ MathParser/MathParser.cpp
@@ -14,14 +14,17 @@
 #include <SDL_image.h>
 #undef main
 
+
 //constexpr double PI = 3.141592741012573242;
 constexpr double PI = 3.14159274;
 constexpr int screen_w = 640;
 constexpr int screen_h = 640;
 constexpr int channels = 3;
-constexpr int grid_spacing = 64;
-constexpr int scale_factor = 150;
+constexpr int grid_spacing = 32;
+constexpr int scale_factor = 0.1;
 constexpr const char* win_title = "Graphing Calc";
+constexpr int32_t g_kRenderDeviceFlags = -1;
+constexpr  int32_t g_kErrorOccurred = -1;
 
 //should i do using namespace std?
 
@@ -94,21 +97,11 @@ namespace parser
         else return false;
     }
 
-    //checks is str is number, checks if it is negitive
     //handls decimal numbers
     bool is_num(const std::string& s)
     {
         int num_found_periods = 0;
-        int i = 0;
-        if (s[0] == '-' && s.size() < 2)
-        {
-            return false;
-        }
-        if (s[0] == '-')
-        {
-            i = 1; //ignore the input number's sign
-        }
-        for (; i < s.size(); i++)
+        for (int i = 0; i < s.size(); i++)
         {
             if (s[i] == '.')
             {
@@ -374,35 +367,223 @@ namespace parser
     }
 }
 
-void create_canvas(uint32_t *data, int spacing, SDL_PixelFormat* fmt)
+namespace disp
+{
+    int32_t e(int32_t result, std::string errorMessage)
+    {
+        if (result)
+            std::cout << errorMessage;
+
+        return result;
+    }
+
+    inline uint32_t ARGB(uint32_t red, uint32_t green, uint32_t blue, uint32_t alpha)
+    {
+        return (alpha << 24) | (red << 16) | (green << 8) | blue;
+    }
+
+    SDL_Window* CreateCenteredWindow(uint32_t width, uint32_t height, std::string title)
+    {
+        // Get current device's Display Mode to calculate window position
+        SDL_DisplayMode DM;
+        SDL_GetCurrentDisplayMode(0, &DM);
+
+        // Calculate where the upper-left corner of a centered window will be
+        const int32_t x = DM.w / 2 - width / 2;
+        const int32_t y = DM.h / 2 - height / 2;
+
+        // Create the SDL window
+        SDL_Window* pWindow = SDL_CreateWindow(win_title, x, y, screen_w, screen_h,
+            SDL_WINDOW_ALLOW_HIGHDPI);
+
+        if (e(!pWindow, "Failed to create Window\n"));
+
+        return pWindow;
+    }
+
+    // Create SDL renderer and configure whether or not to use Hardware Acceleration
+    SDL_Renderer* CreateRenderer(SDL_Window* pWindow, bool hardwareAccelerated)
+    {
+        if (hardwareAccelerated)
+            return SDL_CreateRenderer(pWindow, g_kRenderDeviceFlags, SDL_RENDERER_ACCELERATED);
+        else
+            return SDL_CreateRenderer(pWindow, g_kRenderDeviceFlags, SDL_RENDERER_SOFTWARE);
+    }
+
+    // Create an SDL Texture to use as a "back buffer"
+    SDL_Texture* CreateBackBufferTexture(SDL_Renderer* pRenderer)
+    {
+        SDL_Texture* pTexture = SDL_CreateTexture(pRenderer, SDL_PIXELFORMAT_ARGB8888,
+            SDL_TEXTUREACCESS_STREAMING, screen_w, screen_h);
+
+        if (e(!pTexture, "Failed to create Back Buffer Texture\n"));
+
+        return pTexture;
+    }
+
+    // Free resources 
+    void Shutdown(SDL_Window** ppWindow, SDL_Renderer** ppRenderer, SDL_Texture** ppTexture)
+    {
+        // Free the Back Buffer
+        if (ppTexture)
+        {
+            SDL_DestroyTexture(*ppTexture);
+            *ppTexture = nullptr;
+        }
+
+        // Free the SDL renderer
+        if (ppRenderer)
+        {
+            SDL_DestroyRenderer(*ppRenderer);
+            *ppRenderer = nullptr;
+        }
+
+        // Free the SDL window
+        if (ppWindow)
+        {
+            SDL_DestroyWindow(*ppWindow);
+            *ppWindow = nullptr;
+        }
+    }
+
+    // Initialize SDL Components 
+    int32_t Startup(SDL_Window** ppWindow, SDL_Renderer** ppRenderer, SDL_Texture** ppTexture)
+    {
+        SDL_Init(SDL_INIT_VIDEO);
+
+        if (e(!ppWindow, "Potiner to Window* was null\n")) return -1;
+
+        *ppWindow = CreateCenteredWindow(screen_w, screen_h, win_title);
+
+        if (e(!*ppWindow, "No Window. Aborting..."))
+        {
+            Shutdown(ppWindow, ppRenderer, ppTexture);
+
+            return -1;
+        }
+
+        if (e(!ppRenderer, "Pointer to Renderer* was null\n")) return -1;
+
+        *ppRenderer = CreateRenderer(*ppWindow, true);
+
+        if (e(!ppRenderer, "No Renderer. Aborting..."))
+        {
+            Shutdown(ppWindow, ppRenderer, ppTexture);
+
+            return -1;
+        }
+
+        if (e(!ppTexture, "Pointer to Texture* was null\n")) return -1;
+
+        *ppTexture = CreateBackBufferTexture(*ppRenderer);
+
+        if (e(!*ppTexture, "No back buffer Texture. Aborting..."))
+        {
+            Shutdown(ppWindow, ppRenderer, ppTexture);
+
+            return -1;
+        }
+
+        return 0;
+    }
+
+    // Call this once during each render loop in order to determine when the user wishes to terminate the program
+    bool ProcessInput()
+    {
+        // Return this value to tell the caller whether or not it should continue rendering
+        // We will terminate the application if any key is pressed
+        bool keepRenderLoopRunning = true;
+
+        // Events are generated by SDL whenever something occurs system-wide
+        // We are only interested in keyboard events and when the user closes the window
+        // We will terminate the application if a key is pressed or if the window is manually closed
+        SDL_Event event;
+
+        // Process all events and return whether or not to quit
+        while (SDL_PollEvent(&event))
+        {
+            // Handle relevant SDL events
+            switch (event.type)
+            {
+                // Terminate application if a key is pressed or if the user closes the window
+            case SDL_KEYDOWN:
+            case SDL_QUIT:
+                keepRenderLoopRunning = false;
+            }
+        }
+
+        // Let the caller know if it should continue rendering, otherwise terminate
+        return keepRenderLoopRunning;
+    }
+
+
+    // Call this within every render loop
+    // Fills screen with randomly generated colored pixels
+    int32_t Render(SDL_Window* pWindow, SDL_Renderer* pRenderer, SDL_Texture* pTexture, uint32_t* data)
+    {
+        // The Back Buffer texture may be stored with an extra bit of width (pitch) on the video card in order to properly
+        // align it in VRAM should the width not lie on the correct memory boundary (usually four bytes).
+        int32_t pitch = 0;
+
+        // This will hold a pointer to the memory position in VRAM where our Back Buffer texture lies
+        uint32_t* pPixelBuffer = nullptr;
+
+        // Lock the memory in order to write our Back Buffer image to it
+        if (!SDL_LockTexture(pTexture, NULL, (void**)&pPixelBuffer, &pitch))
+        {
+            // The pitch of the Back Buffer texture in VRAM must be divided by four bytes
+            // as it will always be a multiple of four
+            pitch /= sizeof(uint32_t);
+
+            memcpy(pPixelBuffer, data, screen_h * static_cast<size_t>(pitch)* sizeof(uint32_t));
+
+            // Unlock the texture in VRAM to let the GPU know we are done writing to it
+            SDL_UnlockTexture(pTexture);
+
+            // Copy our texture in VRAM to the display framebuffer in VRAM
+            SDL_RenderCopy(pRenderer, pTexture, NULL, NULL);
+
+            // Copy the VRAM framebuffer to the display
+            SDL_RenderPresent(pRenderer);
+
+            return 0;
+        }
+        else
+            return g_kErrorOccurred;
+    }
+}
+
+void create_canvas(uint32_t *data)
 {
     //create grid  
-    for (int x = 0; x < screen_w/spacing; x += spacing)
+    for (int x = grid_spacing; x < screen_w; x += grid_spacing)
     {
         for (int y = 0; y < screen_h; y++)
         {
-            data[x + (y * screen_w)] = SDL_MapRGB(fmt, 255, 0, 0);
+            //printf("x:%d y:%d\n", x, y);
+            data[x + (y * screen_w)] = disp::ARGB(0, 120, 0, 255);
         }
     }
    
     for (int x = 0; x < screen_w; x++)
     {
-        for (int y = 0; y < screen_h/spacing; y += spacing)
+        for (int y = grid_spacing; y < screen_h; y += grid_spacing)
         {
-            data[x + (y * screen_w)] = SDL_MapRGB(fmt, 255, 0, 0);
+            data[x + (y * screen_w)] = disp::ARGB(0, 120, 0, 255);
         }
     }
-    
+
     //y axis
     for (int y = 0; y < screen_h; y++)
     {
-        data[0 + (y * screen_w)] = SDL_MapRGB(fmt, 255, 0, 0);
+        data[screen_w/2 + (y * screen_w)] = disp::ARGB(137, 0, 0, 255);
     }
     //x axis
     for (int x = 0; x < screen_h; x++)
     {
-        data[x + (0 * screen_w)] = SDL_MapRGB(fmt, 255, 0, 0);
+        data[x + (screen_h/2 * screen_w)] = disp::ARGB(137, 0, 0, 255);
     }
+    
 }
 
 int map_to_screen(int a)
@@ -420,125 +601,90 @@ int map_to_screen(int a)
 
 int main()
 {
-    bool quit = false;
+    bool running = true;
     bool first_iter = true;
 
     uint32_t* data = new uint32_t[screen_w * screen_h];
 
-    SDL_Event event;
-    SDL_Window* window = nullptr;
-    SDL_Renderer* renderer = nullptr;
-    SDL_Texture* texture = nullptr;
-
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        printf("SDL could not initialize SDL_Error: %s\n", SDL_GetError());
-    }
-
-    window = SDL_CreateWindow(win_title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screen_w, screen_h, SDL_WINDOW_SHOWN);
-    if (window == NULL)
-    {
-        printf("Window could not be created SDL_Error: %s\n", SDL_GetError());
-    }
-
-    void* pixels;
-    int pitch;
-    SDL_PixelFormat* fmt;
-    Uint32 format = SDL_GetWindowPixelFormat(window);
-    fmt = SDL_AllocFormat(format);
-    SDL_LockTexture(texture, NULL, &pixels, &pitch);
-    memcpy(pixels, data, screen_h * static_cast<size_t>(pitch));
-
-    //do changes
-    create_canvas(data, grid_spacing, fmt);
-
-    SDL_UnlockTexture(texture);
-    SDL_FreeFormat(fmt);
+    SDL_Window* pWindow = nullptr;
+    SDL_Renderer* pRenderer = nullptr;
+    SDL_Texture* pTexture = nullptr;
 
     std::string var_name = "x";
 
-    while (!quit)
+    create_canvas(data);
+
+    if (disp::e(disp::Startup(&pWindow, &pRenderer, &pTexture), "Startup Failed. Aborting...\n"))
     {
-        
-        if (quit) //change later
+        disp::Shutdown(&pWindow, &pRenderer, &pTexture);
+        return -1;
+    }
+
+    while (running)
+    {
+        if (first_iter)
         {
-            quit = true;
+            disp::Render(pWindow, pRenderer, pTexture, data);
+            std::string a;
+            std::cout << "x is assumed to be a variable\n";
+            std::cout << "make sure to have spaces between every number or operator/parenthese\n\n";
+            std::cout << "input: ";
+            std::getline(std::cin, a);
+            std::cout << '\n';
+            std::vector<std::string> rpn = parser::s_yard(a, "x");
+
+            //plot
+            int c = 0;
+            for (int x = -screen_w / 2; x < screen_w / 2; x++)
+            {
+                float t = screen_w / 2.f * x / 10;
+                double temp = scale_factor * parser::eval_rpn(rpn, var_name, x);
+                std::cout << t << '\n';
+                int y = round(temp);
+                //std::cout << "y:" << y << " x:" << x << '\n';
+                y = std::max(map_to_screen(y), screen_h);
+                
+                if (y > 0)
+                {
+                    data[x + (y * screen_w)] = disp::ARGB(0, 0, 255, 255);
+                }
+            }
+            first_iter = false;
         }
         else
         {
-            if (first_iter)
+            //plot another line, quit, or clear
+            std::string a;
+            std::cout << R"(input "c" to clear the graph, "q" to quit the program, or type another expression(that will be ploted on top): )";
+            std::getline(std::cin, a);
+            if (parser::to_lower(a) == "q")
             {
-                std::string a;
-                std::cout << "x is assumed to be a variable\n";
-                std::cout << "make sure to have spaces between every number or operator/parenthese\n\n";
-                std::cout << "input: ";
-                std::getline(std::cin, a);
-                std::cout << '\n';
-                std::vector<std::string> rpn = parser::s_yard(a, "x");
-
-                //plot
-                int c = 0;
+                std::cout << "quitting...";
+                running = false;
+            }
+            else if (parser::to_lower(a) == "c")
+            {    
+                memset(data, 0, screen_w * screen_h);
+                create_canvas(data);
+            }
+            else
+            {
+                std::vector<std::string>rpn = parser::s_yard(a, var_name);
                 for (int x = -screen_w / 2; x < screen_w / 2; x++)
                 {
                     double temp = scale_factor * parser::eval_rpn(rpn, var_name, x);
                     int y = round(temp);
-                    y = parser::clip(map_to_screen(y), 0, screen_h);
-                    //std::cout << y << '\n';
-                    data[x + (y * screen_w)] = SDL_MapRGB(fmt, 255, 0, 0);
-                }
-                first_iter = false;
-            }
-            else
-            {
-                //plot another line, quit, or clear
-                std::string a;
-                std::cout << R"(input "c" to clear the graph, "q" to quit the program, or type another expression(that will be ploted on top): )";
-                std::getline(std::cin, a);
-                if (parser::to_lower(a) == "q")
-                {
-                    std::cout << "quitting...";
-                    quit = true;
-                }
-                else if (parser::to_lower(a) == "c")
-                {
-                    //reset graph
-                    SDL_PixelFormat* fmt;
-                    Uint32 format = SDL_GetWindowPixelFormat(window);
-                    fmt = SDL_AllocFormat(format);
-                    SDL_RenderClear(renderer);
-                    create_canvas(data, grid_spacing, fmt);
-                    SDL_FreeFormat(fmt);
-                }
-                else
-                {
-                    std::vector<std::string>rpn = parser::s_yard(a, var_name);
-                    for (int x = -screen_w / 2; x < screen_w / 2; x++)
+                    y = std::max(map_to_screen(y), screen_h);
+                    if (y > 0)
                     {
-                        double temp = scale_factor * parser::eval_rpn(rpn, var_name, x);
-                        int y = round(temp);
-                        y = parser::clip(map_to_screen(y), 0, screen_h);
-                        data[x + (y * screen_w)] = SDL_MapRGB(fmt, 255, 0, 0);
+                        data[x + (y * screen_w)] = disp::ARGB(0, 0, 255, 255);
                     }
+                    
                 }
             }
-
-            //apply changes
-            void* pixels;
-            int pitch;
-            fmt = SDL_AllocFormat(format);
-            SDL_LockTexture(texture, NULL, &pixels, &pitch);
-            memcpy(pixels, data, screen_h * static_cast<size_t>(pitch));
-            SDL_UnlockTexture(texture);
-            SDL_FreeFormat(fmt);
-            SDL_RenderCopy(renderer, texture, NULL, NULL);
-            SDL_RenderPresent(renderer);
         }
-
+        disp::Render(pWindow, pRenderer, pTexture, data);
     }
-    SDL_DestroyTexture(texture);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
+     
     return 0;
 }
